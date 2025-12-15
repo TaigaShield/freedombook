c0e715d70c3dadf0aec75f9ee1645a2d19375eb0
Giancarlo Paredes <otakuwalls@gmail.com>
2025-12-15 10:37:16 +0800

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -20,8 +20,6 @@ import { RouterLink, RouterView } from 'vue-router'
 import { loggedIn } from './services/authService';
 import LoginView from './views/LoginView.vue';
 
-// NOTE: holy shit the logout href just works lmao
+// NOTE: holy, the logout href just works lmao
 // because it goes to / and since the username and password are stored in memory it just resets
-// oh btw storing username and password in memory is BAD
-// u should be using JWT but this app is just a proof of concept anyways fuck it we ball
 </script>
